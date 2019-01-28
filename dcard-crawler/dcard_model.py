d89fb78eecfeba2d8112baa63c12b764a4534b72
Jennifer <cutejaneii@hotmail.com>
2019-01-28 23:32:49 +0800

    Create dcard_model.py

---
+++ dcard-crawler/dcard_model.py
@@ -0,0 +1,22 @@
+# encoding=UTF-8
+class dcard_article_model:
+    def __init__(self):
+        self.url=''
+        self.title=''
+        self.content=''
+        self.article_id=0
+        self.date=''
+        self.image_urls=[]
+        self.responses=[]
+        self.image_count=0
+        self.forum_name=''
+        self.forum_alias=''
+
+class dcard_response_model:
+    def __init__(self):
+        self.content=''
+        self.date=''
+        self.likeCount=0
+        self.image_urls=[]
+        self.image_count=0
+    
