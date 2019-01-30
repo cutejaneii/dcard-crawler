ebb2294c8f52c97e09c272d9fcf0d2fff00557e0
Jennifer <cutejaneii@hotmail.com>
2019-01-30 16:38:35 +0800

    Update dcard_main.py

--- dcard-crawler/dcard_main.py
+++ dcard-crawler/dcard_main.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
-from dcard_crawler import dcard_crawl, dcard_crawl_by_keyword
+from test import dcard_crawl, dcard_crawl_by_keyword
 from argparse import ArgumentParser
+import datetime
 
 if __name__ ==  '__main__':
     parser = ArgumentParser()
     parser.add_argument('-o', '--output', help='output document name')
     parser.add_argument('-f', '--forum', default='trending', help='Dcard forum name (default: trending)')
-    parser.add_argument('-a', '--article_id', type=int, default=160000, help='Article id (default: 160000)')
+    parser.add_argument('-a', '--article_id', type=int, default=1, help='Article id (default: 1)')
     parser.add_argument('-m', '--mode', type=int, default=0, help='Query mode (default:0)  0:Most popular articles, 1:Get NEWER articles, 2:get OLDER articles, 3:get articles by KEYWORD ')
-    parser.add_argument('-c', '--count', type=int, default=10, help='Article count')
+    parser.add_argument('-c', '--count', type=int, default=10, help='Article count (default: 10)')
     parser.add_argument('-r', '--get_responses', type=int, default=0, help='Get response (default:0)  0:No responses, 1:With response')
     parser.add_argument('-k', '--keyword', default='pizza', help='Keyword')
     args = parser.parse_args()
@@ -16,7 +17,6 @@ if __name__ ==  '__main__':
         print('output document name='+ args.output)
 
     data=[]
-    article_id=0
 
     check=True
 
@@ -26,19 +26,26 @@ if __name__ ==  '__main__':
 
     if (args.get_responses not in [0, 1]):
         print('[get_responses] can only be 0, 1.')
+        check=False
 
+
+    if (args.count == 0):
+        print('[count] should bigger than 0.')
+        check=False
+
+    k = str(datetime.datetime.now())
 
     if (check==True):
         if (args.mode in [0, 1, 2, 3]):
             if (args.mode==0):
-                article_id, data = dcard_crawl(args.forum, 'true', 1, 1, args.count, args.get_responses)
+                data = dcard_crawl(args.forum, 'true', 1, 1, args.count, args.get_responses)
             else:
                 if (args.mode==1):
-                    article_id, data = dcard_crawl(args.forum, 'false', 1, args.article_id, args.count, args.get_responses)
+                    data = dcard_crawl(args.forum, 'false', 1, args.article_id, args.count, args.get_responses)
                 elif (args.mode==2):
-                    article_id, data = dcard_crawl(args.forum, 'false', args.article_id, 1, args.count, args.get_responses)
+                    data = dcard_crawl(args.forum, 'false', args.article_id, 1, args.count, args.get_responses)
                 else:
-                    article_id, data = dcard_crawl_by_keyword(args.keyword, args.count, args.get_responses)
+                    data = dcard_crawl_by_keyword(args.keyword, args.count, args.get_responses)
 
         for item in data:
             print('['+ str(item.article_id) +'] '+item.title)
