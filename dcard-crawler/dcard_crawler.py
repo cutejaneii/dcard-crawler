4d6fe3f4f968b10f4dbe8e785b6c25e0db1acac5
Jennifer <cutejaneii@hotmail.com>
2019-01-30 16:39:40 +0800

    Update dcard_crawler.py

--- dcard-crawler/dcard_crawler.py
+++ dcard-crawler/dcard_crawler.py
@@ -5,6 +5,9 @@ import json
 import sys
 from bs4 import BeautifulSoup
 from dcard_model import dcard_article_model, dcard_response_model
+import datetime
+import threading
+import Queue
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT10.0; Win64; rv:64.0)', 'charset':'utf-8'}
 
@@ -82,6 +85,35 @@ def get_response(article_id):
 
 def to_model(articles, is_get_response):
     model = []
+    try:
+        for article in articles:
+            board=article['forumAlias']
+
+            article_model = dcard_article_model()
+            article_model.content = get_article_content(board, article['id'])
+
+            article_model.forum_name = article['forumName']
+            article_model.article_id = article['id']
+            article_model.title = article['title']
+            article_model.url = 'https://www.dcard.tw/f/' + board + '/p/'+str(article['id'])
+            article_model.date = article['createdAt']
+            if (article['media'] is None):
+                article_model.image_urls.extend(handleImg(article['mediaMeta']))
+            else:
+                for media_url in article['media']:
+                    article_model.image_urls.append(media_url)
+                article_model.image_count = len(article_model.image_urls)
+                if (is_get_response==1):
+                    article_model.responses.extend(get_response(article['id']))
+            model.append(article_model)
+        pass
+    except Exception as ex:
+        print(ex)
+    return model
+
+
+def to_model_job(articles, is_get_response, q):
+    model = []
     article_id=0
     try:
         for article in articles:
@@ -109,14 +141,50 @@ def to_model(articles, is_get_response):
         pass
     except Exception as ex:
         print(ex)
-    return article_id, model
+    q.put(model) #return results
+    q.task_done()
+
+
+def threading_job(articles, is_get_response):
+    dcard_articles = []
+    try:
+        if (len(articles)>=10):
+            q = Queue.Queue(10)
+            threads=[]
+            each_count = len(articles)//9
+
+            for x in range(0, 9):
+                thread = threading.Thread(target=to_model_job, args=(articles[x*each_count:(x+1)*each_count], is_get_response, q),)
+                thread.setDaemon(True)
+                thread.start()
+                threads.append(thread)
+
+            left = len(articles) - (9*each_count)
+            if (left > 0):
+                thread = threading.Thread(target=to_model_job, args=(articles[9*each_count:], is_get_response, q),)
+                thread.setDaemon(True)
+                thread.start()
+                threads.append(thread)
+
+            for _ in range(len(threads)):
+                q.join()
+
+            for _ in range(len(threads)):
+                dcard_articles.extend(q.get()) # 取出 queue 裡面的資料
+        else:
+            dcard_articles = to_model(articles, is_get_response)
+    except Exception as ex:
+        print(ex)
+    return dcard_articles
+
+
+def take_articleid(element):
+    return element.article_id
 
 
 def dcard_crawl(board, is_popular, before_article_id, after_article_id, limit, is_get_response):
     dcard_articles = []
-    article_id=0
     try:
-
         articles=[]
         crawl_url='https://www.dcard.tw/_api/forums/'+ board +'/posts?popular='+ is_popular +'&limit='+str(limit)
 
@@ -130,37 +198,42 @@ def dcard_crawl(board, is_popular, before_article_id, after_article_id, limit, i
         result = requests.get(crawl_url, headers=headers)
         articles = result.json()
 
-        article_id, dcard_articles = to_model(articles, is_get_response)
+        dcard_articles = threading_job(articles, is_get_response)
+        dcard_articles.sort(key=take_articleid, reverse=True)
 
     except Exception as ex:
         print(str(ex))
 
-    return article_id, dcard_articles
-
+    return dcard_articles
 
 
 def dcard_crawl_by_keyword(keyword, limit, is_get_response):
     dcard_articles = []
-    article_id=0
     try:
         articles=[]
         payload = {'query':keyword, 'highlight':'true', 'limit':limit, 'offset':0, 'since':0}
         if (limit<31):
-            headers2 = {'User-Agent':'Mozilla/5.0 (Windows NT10.0; Win64; rv:64.0)', 'Content-Type':'application/json', 'charset':'utf-8'}
-            articles = requests.get('https://www.dcard.tw/_api/search/posts', params=payload, headers=headers2).json()
+            articles = requests.get('https://www.dcard.tw/_api/search/posts', params=payload, headers=headers).json()
         else:
-            payload = {'query':keyword, 'highlight':'true', 'limit':limit, 'offset':0, 'since':0}
-            articles = requests.get('https://www.dcard.tw/_api/search/posts', params=payload, headers=headers2).json()
+            payload = {'query':keyword, 'highlight':'true', 'limit':30, 'offset':0, 'since':0}
+            articles = requests.get('https://www.dcard.tw/_api/search/posts', params=payload, headers=headers).json()
+            print(payload)
+            print(str(len(articles)))
             for i in xrange(1, (limit//30), 1):
                 payload = {'query':keyword, 'highlight':'true', 'limit':30, 'offset':i*30, 'since':0}
-                articles = requests.get('https://www.dcard.tw/_api/search/posts', params=payload, headers=headers2).json()
+                articles.extend(requests.get('https://www.dcard.tw/_api/search/posts', params=payload, headers=headers).json())
+                print(payload)
+                print(str(len(articles)))
 
             if (limit%30 > 0):
                 payload = {'query':keyword, 'highlight':'true', 'limit':limit%30, 'offset':len(articles), 'since':0}
-                articles = requests.get('https://www.dcard.tw/_api/search/posts', params=payload, headers=headers2).json()
- 
-        article_id, dcard_articles = to_model(articles, is_get_response)
+                articles.extend(requests.get('https://www.dcard.tw/_api/search/posts', params=payload, headers=headers).json())
+                print(payload)
+                print(str(len(articles)))
+
+        dcard_articles = threading_job(articles, is_get_response)
+        dcard_articles.sort(key=take_articleid, reverse=True)
 
     except Exception as ex:
         print(str(ex))
-    return article_id, dcard_articles
+    return dcard_articles
